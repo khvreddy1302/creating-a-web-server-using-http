3d6870a4bb436a9a0a47e17d1ff17b736db2177e
khvreddy1302 <kasaramharshavardhan@gmail.com>
2024-07-04 12:50:55 +0530

    Update http request.js

--- http request.js
+++ http request.js
@@ -1,5 +1,5 @@
 const request=require('request')
-const url='https://api.weatherstack.com/current?access_key=eb39ec3f5733433ee8c0f5abcf390fb3&query=37.8267,-122.4233'
+const url='API KEY'
 
 request({url:url},(Error,Response)=>{
     // console.log(Response)
