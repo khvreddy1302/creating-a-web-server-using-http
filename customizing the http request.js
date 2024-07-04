c94abcc7f91c9a4a423f8daeb478747671cf4f0c
khvreddy1302 <kasaramharshavardhan@gmail.com>
2024-07-04 12:52:23 +0530

    Update customizing the http request.js

--- customizing the http request.js
+++ customizing the http request.js
@@ -1,9 +1,12 @@
 // challenge to print the current temperature and precipitation for chance of the rain
+// converting the temperature from celsius to fahrenheit
 
 const request=require('request')
-const url='http://api.weatherstack.com/current?access_key=e77e01e61ed600911dbe4572500d43ba&query=New%20York'
+const url='http://api.weatherstack.com/current?access_key=e77e01e61ed600911dbe4572500d43ba&query=New%20York&units=f'
 
 request({url:url, json:true},(error,response)=>{
+    console.log(response.body.current)
     console.log("It is currently "+ response.body.current.temperature +" degrees out " +"There is "+ response.body.current.precip ," chance of rain")
-
+    console.log("It is currently "+response.body.current.temperature+" degrees out "+"feels like "+response.body.current.feelslike+" outside.")
+    
 } )
